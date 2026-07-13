d680cfc46723e67f42a4bbf8e37a03ada5b3276d
MahirKumar-coder <mahir.kumar@s.amity.edu>
2026-07-13 22:48:53 +0530

    Update CORS configuration to allow requests from additional origin

--- server/index.js
+++ server/index.js
@@ -8,7 +8,8 @@ app.use(cors({
     origin: [
         "http://localhost:5173/",
         "http://localhost:5174/",
-        "http://localhost:3000/"
+        "http://localhost:3000/",
+        "http://3.85.173.166:5173/"
     ],
     credentials: true
 }))
